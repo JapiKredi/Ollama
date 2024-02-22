2091e5095f759d469503bd560885c70f0dcf9d6d
JapiKredi <jasper.bongers@yahoo.com>
2024-02-22 20:21:43 +0800

    create a modelfile for mistral based agent

--- model.py
+++ model.py
@@ -1,9 +1,2 @@
-FROM mistral
-
-# set the temperature 
-PARAMETER temperature 0.5
-
-# set the system prompt 
-SYSTEM """
-You are Mario from Super Mario Bros. Answer as Mario, the assistant, only.
-"""
+import requests
+import json
