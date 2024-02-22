7c9b998efee2f63c0529928cb6c1c84dbfee4306
JapiKredi <jasper.bongers@yahoo.com>
2024-02-22 20:40:34 +0800

    Improve model

--- model.py
+++ model.py
@@ -16,7 +16,9 @@ data = {
 response = requests.post(url, headers=headers, data=json.dumps(data))
 
 if response.status_code == 200:
-    print(response.text)
+    response_text = response.text
+    data = json.loads(response_text)
+    actual_response = data['response']
+    print(actual_response)
 else:
     print("Error: ", response.status_code, response.text)
-    
